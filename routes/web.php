53c91d06aa5dfe943dc5bfb2432cbe327e63b453
luguisaca <lgsalamanca7@misena.edu.co>
2018-11-05 16:16:08 -0500

    cambios par ejemplos test

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//pruebas realizadas de la web https://styde.net/rutas-con-laravel/
+
+Route::get('/usuarios', function () {
+    return 'Imprime en pantalla la palabra "usuarios"';
+});
+// usando una función anónima
+Route::get('/url', function () {
+    return 'Hola mundo';
+});
+//Rutas con parametros, donde le indicamos que parametro nos muestre.
+Route::get('users/{id}', function ($id) {
+    return'Usuarios';
+});
 
